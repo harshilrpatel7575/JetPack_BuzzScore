94c007a82e21a477d3f62e3400edd526fcda80b0
Harshil Patel <harshilrpatel7575@gmail.com>
2024-07-07 15:38:24 +0530

    first commit

--- app/src/main/java/com/example/buzzscore/data/remote/models/ElenaApiService.kt
+++ app/src/main/java/com/example/buzzscore/data/remote/models/ElenaApiService.kt
@@ -1,14 +1,16 @@
 package com.example.buzzscore.data.remote.models
 
-import com.example.buzzscore.util.GET_INPLAY_FIXTURES
-import com.example.buzzscore.util.GET_UPCOMING_MATCHES
+import com.example.buzzscore.util.API_TOKEN
+import com.example.buzzscore.util.GET_MATCHES_ENDPOINT
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ElenaApiService {
 
-    @GET(GET_INPLAY_FIXTURES)
-    suspend fun getInplayMatches(): InplayMatchesResponse
 
-    @GET(GET_UPCOMING_MATCHES)
-    suspend fun getUpcomingMatches(): UpcomingMatchesResponse
+
+    @GET(GET_MATCHES_ENDPOINT)
+    suspend fun getMatches(
+        @Query("apikey") apikey: String = API_TOKEN
+    ): UpcomingMatchesResponse
 }
--- app/src/main/java/com/example/buzzscore/util/Constants.kt
+++ app/src/main/java/com/example/buzzscore/util/Constants.kt
@@ -1,6 +1,5 @@
 package com.example.buzzscore.util
 
-const val BASE_URL = ""
-const val GET_INPLAY_FIXTURES = ""
-const val ACCESS_TOKEN = ""
-const val GET_UPCOMING_MATCHES = ""
+const val BASE_URL = "https://api.sportmonks.com/v3/"
+const val GET_MATCHES_ENDPOINT = "football/schedules/seasons/19686?"
+const val API_TOKEN = "MW5rHj3BT9slvAP2NNLf0JUhOrF8ZWoX9MScbgbZMg0s4vRfoMmC6yfrs3Ic"
